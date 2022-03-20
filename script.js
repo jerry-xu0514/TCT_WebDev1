b62943b95ab647dc8e276e853f962e8e46136a39
Hebisann10 <94259642+Sachi2631@users.noreply.github.com>
2022-03-20 04:00:46 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var Name = document.getElementById("name");
+var Email = document.getElementById("email");
+var Subject = document.getElementById("subject");
+
+function info(name, email, subject) {
+  var returnObject = {
+    name: name.value,
+    subject: subject.value,
+    email: email.value
+  }
+  return returnObject
+}
+
+function submitted() {
+  var infoReturnedObject = info(Name, Email, Subject)
+  console.log("Your name is: " + infoReturnedObject.name)
+  console.log("Your e-mail is: " + infoReturnedObject.email)
+  console.log("The issue is: " + infoReturnedObject.subject)
+  document.getElementById('pageOne').style.display = 'none';
+  document.getElementById('pageTwo').style.display = 'block';
+}
+
+function return() {
+  document.getElementById('pageOne').style.display = 'block';
+  document.getElementById('pageTwo').style.display = 'none';
+}
